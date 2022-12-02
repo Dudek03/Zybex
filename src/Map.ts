fe415a79b51d88442174510067beac86074e3c8e
Dudek03 <84042658+Dudek03@users.noreply.github.com>
2022-12-02 12:07:07 +0100

    Update Map.ts

--- src/Map.ts
+++ src/Map.ts
@@ -11,12 +11,12 @@ class Map {
     constructor(stageNum: number, position: position, canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D | null) {
         this.stageNum = stageNum
         this.position = position
-        this.image = createImage(`/level${stageNum}/bgMap.png`)
+        this.image = createImage(`./level${stageNum}/bgMap.png`)
         this.context = ctx
         this.canvas = canvas
     }
     changeStage(stageNum: number): void {
-        this.image = createImage(`/level${stageNum}/bgMap.png`)
+        this.image = createImage(`./level${stageNum}/bgMap.png`)
     }
     draw(): void {
         this.context!.drawImage(this.image, this.position.x, this.position.y, 7500, this.canvas.height / 2)
